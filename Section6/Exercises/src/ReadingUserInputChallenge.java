ffa50737dc87fd7d186ff1d413594665d384d586
Vegator <vegator@tutanota.com>
2023-10-07 14:48:22 +0100

    Complete interactive Programme exercise

--- Section6/Exercises/src/ReadingUserInputChallenge.java
+++ Section6/Exercises/src/ReadingUserInputChallenge.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ReadingUserInputChallenge {
 
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        int counter = 1;
+        double sum = 0;
+
+        while (counter <= 5) {
+            System.out.println("Enter number #" + counter + ":");
+            String nextNumber = scanner.nextLine();
+            try {
+                //int number = Integer.parseInt(nextNumber); // enable decimals!
+                double number = Double.parseDouble(nextNumber);
+                counter++;
+                sum += number;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number");
+            }
+
+        }
+        System.out.println("The sum of the 5 numbers = " + sum);
+    }
 }
